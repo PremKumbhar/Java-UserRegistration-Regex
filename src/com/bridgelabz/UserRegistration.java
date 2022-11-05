291c413974a0175cf096afafa00db22a4aae2adc
PremKumbhar <kumbharprem7738@gmail.com>
2022-11-05 13:53:58 +0530

    [Prem]:Added validate a given emails

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserRegistration {
@@ -7,6 +8,7 @@ public class UserRegistration {
         System.out.println("User registration problem");
         Scanner sc = new Scanner(System.in);
         UserRegistrationInput userRegistration = new UserRegistrationInput();
+        ArrayList<String> emails = new ArrayList<>();
 
         System.out.println("Enter first name ");
         String firstName = sc.next();
@@ -42,5 +44,39 @@ public class UserRegistration {
             System.out.println("Valid password");
         else
             System.out.println("Invalid password");
+
+        // Valid Emails
+        emails.add("abc@yahoo.com");
+        emails.add("abc-100@yahoo.com");
+        emails.add("abc.100@yahoo.com");
+        emails.add("abc111@abc.com");
+        emails.add("abc-100@abc.net");
+        emails.add("abc.100@abc.com.au");
+        emails.add("abc@1.com");
+        emails.add("abc@gmail.com.com");
+        emails.add("abc+100@gmail.com");
+
+        // Invalid Emails
+        emails.add("abc");
+        emails.add("abc@.com.my");
+        emails.add("abc123@gmail.a");
+        emails.add("abc123@.com");
+        emails.add("abc123@.com.com");
+        emails.add(".abc@abc.com");
+        emails.add("abc()*@gmail.com");
+        emails.add("abc@%*.com");
+        emails.add("abc..2002@gmail.com");
+        emails.add("abc.@gmail.com");
+        emails.add("abc@abc@gmail.com");
+        emails.add("abc@gmail.com.1a");
+        emails.add("abc@gmail.com.aa.au");
+
+        for (String mail : emails){
+            if (userRegistration.isEmailValid(mail))
+                System.out.println(mail + " Valid Email");
+            else
+                System.out.println(mail + " Invalid Email");
+
+        }
     }
 }
