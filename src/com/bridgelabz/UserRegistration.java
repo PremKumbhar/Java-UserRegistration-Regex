0e36253878d9009b6ce012d48543adb70e879a1d
PremKumbhar <kumbharprem7738@gmail.com>
2022-11-05 13:23:31 +0530

    [Prem]:Added user need to enter valid first name

---
+++ src/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("User registration problem");
+        Scanner sc = new Scanner(System.in);
+        UserRegistrationInput userRegistration = new UserRegistrationInput();
+
+        System.out.println("Enter first name ");
+        String firstName = sc.next();
+        if (userRegistration.isFirstNameValid(firstName))
+            System.out.println("Valid first name");
+        else
+            System.out.println("Invalid first name");
+    }
+}
